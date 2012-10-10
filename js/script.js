95f231bd6473b7a59c8a82169d32353a2dcda7c5
Tak Tran <contact@tutaktran.com>
2012-10-10 18:49:43 +0100

    Allow for movies.

--- js/script.js
+++ js/script.js
@@ -2,7 +2,7 @@ _.mixin(_.string.exports());
 
 var App = App || {};
 
-App.guessWords = [
+App.cityWords = [
     'Shanghai',
     'Istanbul',
     'Karachi',
@@ -69,9 +69,245 @@ App.guessWords = [
     'Jaipur'
   ];
 
+App.movieWords = [
+  'The Shawshank Redemption',
+  'The Godfather',
+  'Pulp Fiction',
+  'The Good, the Bad and the Ugly',
+  '12 Angry Men',
+  'Schindler\'s List',
+  'The Dark Knight',
+  'The Lord of the Rings',
+  'Fight Club',
+  'The Empire Strikes Back',
+  'One Flew Over the Cuckoo\'s Nest',
+  'Inception',
+  'Goodfellas',
+  'Star Wars',
+  'Seven Samurai',
+  'The Matrix',
+  'Forrest Gump',
+  'City of God',
+  'Once Upon a Time in the West',
+  'Casablanca',
+  'The Silence of the Lambs',
+  'Raiders of the Lost Ark',
+  'The Usual Suspects',
+  'Rear Window',
+  'The Dark Knight Rises',
+  'Psycho',
+  'It\'s a Wonderful Life',
+  'Sunset Boulevard',
+  'Memento',
+  'American History X',
+  'Apocalypse Now',
+  'Terminator 2',
+  'Dr. Strangelove',
+  'Saving Private Ryan',
+  'North by Northwest',
+  'City Lights',
+  'Spirited Away',
+  'Citizen Kane',
+  'The Shining',
+  'Modern Times',
+  'American Beauty',
+  'Taxi Driver',
+  'Vertigo',
+  'Back to the Future',
+  'The Pianist',
+  'The Departed',
+  'Toy Story 3',
+  'Paths of Glory',
+  'Double Indemnity',
+  'Aliens',
+  'Life Is Beautiful',
+  'WALL E',
+  'The Lives of Others',
+  'A Clockwork Orange',
+  'Amelie',
+  'Lawrence of Arabia',
+  'To Kill a Mockingbird',
+  'Gladiator',
+  'The Green Mile',
+  'The Prestige',
+  'The Great Dictator',
+  'Reservoir Dogs',
+  'Untouchable',
+  'Das Boot',
+  'Requiem for a Dream',
+  'The Third Man',
+  'The Treasure of the Sierra Madre',
+  'Eternal Sunshine of the Spotless Mind',
+  'Cinema Paradiso',
+  'Chinatown',
+  'Once Upon a Time in America',
+  'L.A. Confidential',
+  'Full Metal Jacket',
+  'Return of the Jedi',
+  'Monty Python and the Holy Grail',
+  'The Lion King',
+  'Braveheart',
+  'Some Like It Hot',
+  'Singing in the Rain',
+  'Oldboy',
+  'Amadeus',
+  'Metropolis',
+  'Rashomon',
+  'Bicycle Thieves',
+  'Unforgiven',
+  'All About Eve',
+  '2001',
+  'The Apartment',
+  'Raging Bull',
+  'Indiana Jones and the Last Crusade',
+  'The Bridge on the River Kwai',
+  'The Sting',
+  'A Separation',
+  'Die Hard',
+  'Pan\'s Labyrinth',
+  'Downfall',
+  'Batman Begins',
+  'Witness for the Prosecution',
+  'Mr Smith Goes to Washington',
+  'Grave of the Fireflies',
+  'Avengers Assemble',
+  'The Great Escape',
+  'Yojimbo',
+  'For a Few Dollars More',
+  'Inglourious Basterds',
+  'Snatch',
+  'The Elephant Man',
+  'On the Waterfront',
+  'The Seventh Seal',
+  'The Maltese Falcon',
+  'Toy Story',
+  'Gran Torino',
+  'The General',
+  'Rebecca',
+  'Blade Runner',
+  'Wild Strawberries',
+  'Touch of Evil',
+  'The Kid',
+  'The Big Lebowski',
+  'Scarface',
+  'The Deer Hunter',
+  'Cool Hand Luke',
+  'Sin City',
+  'Strangers on a Train',
+  'The Gold Rush',
+  'No Country for Old Men',
+  'It Happened One Night',
+  'High Noon',
+  'Hotel Rwanda',
+  'The Sixth Sense',
+  'Lock, Stock and Two Smoking Barrels',
+  'Platoon',
+  'Butch Cassidy and the Sundance Kid',
+  'The Thing',
+  'The Wizard of Oz',
+  'Trainspotting',
+  'Kill Bill: Vol. 1',
+  'Casino',
+  'Notorious',
+  'Warrior',
+  'Annie Hall',
+  'The King\'s Speech',
+  'The Secret in Their Eyes',
+  'The Grapes of Wrath',
+  'Gone with the Wind',
+  'Into the Wild',
+  'Good Will Hunting',
+  'Life of Brian',
+  'Black Swan',
+  'Finding Nemo',
+  'The Big Sleep',
+  'How to Train Your Dragon',
+  'V for Vendetta',
+  'Ben Hur',
+  'My Neighbour Totoro',
+  'Network',
+  'The Terminator',
+  'Donnie Darko',
+  'Dial M for Murder',
+  'The Night of the Hunter',
+  'Million Dollar Baby',
+  'Stand by Me',
+  'There Will Be Blood',
+  'Groundhog Day',
+  'Dog Day Afternoon',
+  'Twelve Monkeys',
+  'Amores Perros',
+  'The Bourne Ultimatum',
+  'The Artist',
+  'The Graduate',
+  'Mary and Max',
+  'The 400 Blows',
+  'Gandhi',
+  'Persona',
+  'The Killing',
+  'Slumdog Millionaire',
+  'The Princess Bride',
+  'Howl\'s Moving Castle',
+  'Who\'s Afraid of Virginia Woolf?',
+  'The Manchurian Candidate',
+  'La Strada',
+  'The Hustler',
+  'The Wild Bunch',
+  'Stalag 17',
+  'A Beautiful Mind',
+  'Anatomy of a Murder',
+  'The Exorcist',
+  'District 9',
+  'Barry Lyndon',
+  'The Man Who Shot Liberty Valance',
+  'Stalker',
+  'Ratatouille',
+  'Roman Holiday',
+  'Infernal Affairs',
+  'The Truman Show',
+  'The Diving Bell and the Butterfly',
+  'Harry Potter',
+  'A Streetcar Named Desire',
+  'Pirates of the Caribbean',
+  'Ip Man',
+  'Star Trek',
+  'A Fistful of Dollars',
+  'The Wrestler',
+  'Rosemary\'s Baby',
+  'Beauty and the Beast',
+  'Monsters, Inc.',
+  'Looper',
+  'Harvey',
+  'Moonrise Kingdom',
+  'In the Name of the Father',
+  'Nausica of the Valley of the Wind',
+  'Bringing Up Baby',
+  'All Quiet on the Western Front',
+  'Nosferatu',
+  'La Haine',
+  'Shadow of a Doubt',
+  'Manhattan',
+  'Rain Man',
+  'Shutter Island',
+  'Mystic River',
+  'Let the Right One In',
+  'Big Fish',
+  'In the Heat of the Night',
+  'Arsenic and Old Lace',
+  'The Untouchables',
+  'The Nightmare Before Christmas',
+  'The Searchers',
+  'Papillon',
+  'Rio Bravo',
+  'King Kong',
+  'Patton',
+  'Jurassic Park',
+  'The Philadelphia Story',
+  'Ed Wood'
+];
+
 App.flickrSearchUrl = function(searchTerm) {
   return "http://api.flickr.com/services/rest/?method=flickr.photos.search&api_key=0bff7d4c04b7da09b1c294dfcb7da49b&text="  + searchTerm + "+landmarks&format=json&content_type=1";
-
 };
 
 App.flickrImageUrl = function(imageObj) {
@@ -89,7 +325,8 @@ App.scoring = {
 
 App.gameStateDefaults = {
   currentWord: "",
-  imageObjs: []
+  imageObjs: [],
+  words: App.cityWords
 };
 
 App.points = {
@@ -142,6 +379,19 @@ App.points = {
 
 App.initGameState = function() {
   App.gameState = App.gameStateDefaults;
+  App.switchWords();
+};
+
+App.switchWords = function() {
+  var wordListIndex = $("#categories .active").prev().length;
+  switch(wordListIndex) {
+  case 0: // Cities
+    App.gameState.words = App.cityWords;
+    break;
+  case 1: // Movies
+    App.gameState.words = App.movieWords;
+    break;
+  }
 };
 
 App.init = function() {
@@ -158,6 +408,11 @@ App.init = function() {
     App.points.giveUp();
 
     App.showAnswerAlert();
+    App.nextGame();
+  });
+
+  $("#categories button").click(function(event) {
+    App.switchWords();
     App.nextGame();
   });
 };
@@ -179,7 +434,7 @@ App.rndNumber = function(max) {
 App.nextGame = function() {
   App.initGameState();
 
-  var words = App.guessWords,
+  var words = App.gameState.words,
       rndNum = App.rndNumber(words.length),
       word = words[rndNum];
   $("#guess").val("");
