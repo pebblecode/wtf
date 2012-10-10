967a2f3485c5291f8c2167d2ddd48b0d55bd23ea
Tak Tran <contact@tutaktran.com>
2012-10-10 16:27:10 +0100

    Flickr integration

--- js/script.js
+++ js/script.js
@@ -2,43 +2,142 @@ _.mixin(_.string.exports());
 
 var App = App || {};
 
-App.guessWords = {
-  "cocktail": {
-    bannedWords: [
-      "drink",
-      "alcohol",
-      "glass"
-    ],
-    image: "http://rowingjournal.com/site/wp-content/uploads/2012/06/Cocktail.jpg"
-  },
-  "texas": {
-    bannedWords: [
-      "map",
-      "USA",
-      "bbq"
-    ],
-    image: "http://www.sircon.com/state-information-center/images/sic-texas.jpg"
-  }
+App.guessWords = [
+    'Shanghai',
+    'Istanbul',
+    'Karachi',
+    'Mumbai',
+    'Moscow',
+    'Beijing',
+    'Sao Paulo',
+    'Tianjin',
+    'Guangzhou',
+    'Delhi',
+    'Seoul',
+    'Shenzhen',
+    'Jakarta',
+    'Tokyo',
+    'Mexico City',
+    'Kinshasa',
+    'Tehran',
+    'Bangalore',
+    'New York City',
+    'Dongguan',
+    'London',
+    'Lagos',
+    'Lima',
+    'Bogotá',
+    'Ho Chi Minh City',
+    'Hong Kong',
+    'Bangkok',
+    'Dhaka',
+    'Hyderabad',
+    'Cairo',
+    'Hanoi',
+    'Wuhan',
+    'Rio de Janeiro',
+    'Lahore',
+    'Ahmedabad',
+    'Baghdad',
+    'Riyadh',
+    'Singapore',
+    'Santiago',
+    'Saint Petersburg',
+    'Chennai',
+    'Chongqing',
+    'Kolkata',
+    'Surat',
+    'Yangon',
+    'Ankara',
+    'Alexandria',
+    'Shenyang',
+    'Suzhou',
+    'New Taipei',
+    'Johannesburg',
+    'Los Angeles',
+    'Yokohama',
+    'Abidjan',
+    'Busan',
+    'Berlin',
+    'Cape Town',
+    'Durban',
+    'Jeddah',
+    'Pyongyang',
+    'Madrid',
+    'Nairobi',
+    'Pune',
+    'Jaipur'
+  ];
+
+App.flickrSearchUrl = function(searchTerm) {
+  return "http://api.flickr.com/services/rest/?method=flickr.photos.search&api_key=0bff7d4c04b7da09b1c294dfcb7da49b&tags="  + searchTerm + "&format=json";
+
+};
+
+App.flickrImageUrl = function(imageObj) {
+  return "http://farm" + imageObj.farm + ".static.flickr.com/" +
+        imageObj.server + "/" + imageObj.id + "_" + imageObj.secret + "_" + "t.jpg";
+
 };
 
 App.gameState = {
-  currentWord: ""
-}
+  currentWord: "",
+  imageObjs: []
+};
 
 App.init = function() {
   App.nextGame();
+
+  $("#next-image").click(function(event) {
+    App.showRandomImage();
+  });
 };
 
 App.nextGame = function() {
-  var words = _.keys(App.guessWords),
-      rndNum = Math.floor(Math.random() * words.length),
-      word = words[rndNum],
-      image = App.guessWords[word].image;
+  App.gameState = {};
 
-  $("#image").html("<img src='" + image + "'/>");
+  var words = App.guessWords,
+      rndNum = Math.floor(Math.random() * words.length),
+      word = words[rndNum];
+  $("#guess").val("");
+
   App.gameState.currentWord = word;
-  $("#image").hide().slideDown(500);
+  console.log(word);
+
+  $.get(App.flickrSearchUrl(word));
 };
+
+App.showNextImage = function() {
+  App.showRandomImage();
+};
+
+function jsonFlickrApi(data) {
+  console.log("jsonFlickrApi", data);
+
+  App.gameState.imageObjs = data.photos.photo;
+  App.showRandomImage();
+}
+
+App.showRandomImage = function() {
+  var imageUrl = App.randomImageUrl();
+
+  $("#image").html("<img src='" + imageUrl + "'/>");
+  $("#image").hide().slideDown(500);
+}
+
+App.randomImageUrl = function() {
+  var image = App.randomImage(),
+      imageUrl = App.flickrImageUrl(image);
+
+  return imageUrl;
+}
+
+App.randomImage = function() {
+  var numImages = App.gameState.imageObjs.length,
+      rndNum = Math.floor(Math.random() * numImages);
+
+  return App.gameState.imageObjs[rndNum];
+}
 
 $(document).ready(function() {
   $("#guess").keypress(function(event) {
